180dd2d9b53ce3f132b55d0e622fd2f201ddd1e3
Chris Jantzen <jantzeca@mail.uc.edu>
2020-04-10 21:56:36 -0400

    change link

--- client/src/components/Layout/SignedOutLinks.js
+++ client/src/components/Layout/SignedOutLinks.js
@@ -2,16 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import './styles/signedOutLinks.scss';
 
-const SignedOutLinks = () => {
-  return (
-    <ul>
-      <li>
-        <Link to='/signup' className='btn btn-secondary'>
-          Log In
-        </Link>
-      </li>
-    </ul>
-  )
-}
+const SignedOutLinks = () => (
+  <ul>
+    <li>
+      <Link to='/signin' className='btn btn-secondary'>
+        Log In
+      </Link>
+    </li>
+  </ul>
+)
 
 export default SignedOutLinks
