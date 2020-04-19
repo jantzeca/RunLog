6b90ed1c09f061c28eda9701ca69deed3b6eee41
Chris Jantzen <jantzeca@mail.uc.edu>
2020-04-18 23:28:42 -0400

    Remove explicit return statement

--- client/src/components/Error/ErrorAlert.js
+++ client/src/components/Error/ErrorAlert.js
@@ -3,16 +3,14 @@ import PropTypes from 'prop-types';
 
 import './styles/errorAlert.scss';
 
-const ErrorAlert = ({ message }) => {
-  return (
-    <div className='error'>
-      <div className='text'>
-        <h1>Error:</h1>
-        <p>{message || 'An error occurred'}</p>
-      </div>
+const ErrorAlert = ({ message }) => (
+  <div className='error'>
+    <div className='text'>
+      <h1>Error:</h1>
+      <p>{message || 'An error occurred'}</p>
     </div>
-  );
-};
+  </div>
+);
 
 ErrorAlert.propTypes = {
   message: PropTypes.string
